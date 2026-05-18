67e0048ca1a27d62348b0a9e593223bfab63fac8
yonatanperel <146644376+yonatanperel@users.noreply.github.com>
2026-05-18 13:34:23 +0300

    Harbor dweller (#14)
    * new variant - harbor-dweller
    
    * nay-dweller theme + fixing harbor-dweller
    
    ---------
    
    Co-authored-by: yonatan.perel@pointfive.co <146644376+yonatan-perel@users.noreply.github.com>

---
+++ lua/lake-dweller/variants/harbor-dweller.lua
@@ -0,0 +1,39 @@
+return {
+    -- UI
+    bg = "#1e2030",
+    fg = "#cdd6f4",
+    fg_dim = "#7a8194",
+    fg_inverse = "#1e2030",
+    bg_alt = "#272a3d",
+    selection = "#3a4060",
+    search = "#3a4060",
+    search_current = "#7aa2f7",
+    search_inc = "#e0af68",
+    float_bg = "#2a2d40",
+
+    -- Syntax
+    comment = "#9ec396",
+    keyword = "#8b95a0",
+    func = "#7aa2f7",
+    type = "#d4be7e",
+
+    -- Constants
+    string = "#e09778",
+    constant = "#e06c75",
+
+    -- Diagnostics
+    error = "#e06c75",
+    warn = "#e0af68",
+    hint = "#7aa2f7",
+    info = "#cdd6f4",
+
+    -- Virtual text
+    virtual_text = "#8b95a0",
+
+    -- Diff/Git
+    added = "#9ec396",
+    changed = "#7aa2f7",
+    deleted = "#e06c75",
+    selection_added = "#1f2e23",
+    selection_deleted = "#3a2225",
+}
---
+++ lua/lake-dweller/variants/nay-dweller.lua
@@ -0,0 +1,39 @@
+return {
+    -- UI
+    bg = "#062329",
+    fg = "#d3b58d",
+    fg_dim = "#7a6850",
+    fg_inverse = "#062329",
+    bg_alt = "#0b3335",
+    selection = "#1a3478",
+    search = "#1a3478",
+    search_current = "#fdc35e",
+    search_inc = "#fdc35e",
+    float_bg = "#0b3335",
+
+    -- Syntax
+    comment = "#44b340",
+    keyword = "#c1d1e3",
+    func = "#fdc35e",
+    type = "#d8a070",
+
+    -- Constants
+    string = "#2ec09c",
+    constant = "#7ad0c6",
+
+    -- Diagnostics
+    error = "#e06c75",
+    warn = "#fdc35e",
+    hint = "#9ed697",
+    info = "#d3b58d",
+
+    -- Virtual text
+    virtual_text = "#7a6850",
+
+    -- Diff/Git
+    added = "#9ed697",
+    changed = "#88aabb",
+    deleted = "#e06c75",
+    selection_added = "#1a3a2a",
+    selection_deleted = "#3a2225",
+}
